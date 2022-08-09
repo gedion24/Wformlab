17b7ca4a669be87554334de59c3c63d121cea36f
gedion24 <57101143+gedion24@users.noreply.github.com>
2022-08-09 10:13:55 +0300

    Initial commit

---
+++ model/model.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WformLab.model
+{
+    class Item
+    {
+        static List<Item> product = new List<Item>();
+        public string number { get; set; }
+        public string date { get; set; }
+        public string inventorynumber { get; set; }
+        public string objectname { get; set; }
+        public int count { get; set; }
+        public string price { get; set; }
+
+        public bool chbox { get; set; }
+/*
+        public static Item findone (string name)
+        {
+            return product.Find(p => p.name == name);
+        }*/
+
+        static public List<Item> getall()
+        {
+            return product;
+ 
+
+        }
+          
+      
+        public void save()
+        {
+           // Item.add(this);
+        }
+
+    }
+}
---
+++ model/models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WformLab.models
+{
+     class item
+    {
+        static List<item> products = new List<item>();
+        public string Studentname { get; set; }
+        public string StudentID { get; set; }
+        public string Coursename { get; set; }
+       
+   
+        
+        
+        static public List<item> getall()
+        {
+            return products;
+        }
+    }
+
+   
+}
